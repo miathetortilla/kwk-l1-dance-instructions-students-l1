26fd231ea6071a5e7ebc8dcb432dedaa30894f05
miathetortilla <mia.lamontagne@gmail.com>
2018-06-04 22:38:43 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,59 @@
+puts "This is how to dance Gangnam Style"
 
-# Code your methods below
+def lower_body
+  puts "Assume the proper stance."
+  puts "Step 1: Plant legs far apart, bend knees slightly and keep posture loose"
+  puts "Base Footwork"
+  puts "Step 1: Lift right foot"
+  puts "Step 2: Return right foot to the ground"
+  puts "Step 3: Finishing with a small skip-step backward"
+end
+
+def skip_step
+ puts "Step 1: Lower left foot to the ground"
+ puts "Step 2: Bounce left foot back up slightly, kicking it a few inches back"
+end
+
+def upper_body1
+  puts "Step 1: Holding your arms out in front of you, straight and at chest level"
+  puts "Step 2: Cross your right wrist over your left and hold them together"
+  puts "Step 3: Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
+
+def upper_body2
+  puts "Step 1: Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Step 2: Lift right arm up and point right elbow diagonally to the right."
+  puts "Step 3: Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+def bust_a_move
+  puts "Be snazzy!"
+end
+
+lower_body
+puts "Skip Step Left"
+skip_step
+puts "Left, Right, Left and Left"
+
+puts "Skip Step Right"
+skip_step
+puts "Right, Left, Right, and Right"
+
+upper_body1
+upper_body1
+upper_body1
+upper_body1
+upper_body1
+upper_body1
+upper_body1
+upper_body1
+
+upper_body2
+upper_body2
+upper_body2
+upper_body2
+upper_body2
+upper_body2
+upper_body2
+upper_body2
+
